f92c4a1e21686bd59e7be6714ec7fdebf8d31caf
tomat <tomat@406a0c4d-033a-0410-8de8-e80135713968>
2013-12-08 07:13:36 +0000

    merged rel20 branch (up to r4395) back into rel21
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/branches/rel21@4397 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -13818,8 +13818,9 @@ SphWordID_t	CSphDictStarV8::GetWordID ( BYTE * pWord )
 
 	bool bHeadStar = ( pWord[0]=='*' );
 	bool bTailStar = ( pWord[iLen-1]=='*' ) && ( iLen>1 );
+	bool bMagic = ( pWord[0]<' ' );
 
-	if ( !bHeadStar && !bTailStar )
+	if ( !bHeadStar && !bTailStar && !bMagic )
 	{
 		if ( m_pDict->GetSettings().m_bStopwordsUnstemmed && IsStopWord ( pWord ) )
 			return 0;
@@ -13840,7 +13841,12 @@ SphWordID_t	CSphDictStarV8::GetWordID ( BYTE * pWord )
 	if ( !iLen || ( bHeadStar && iLen==1 ) )
 		return 0;
 
-	if ( m_bInfixes )
+	if ( bMagic ) // pass throu MAGIC_* words
+	{
+		memcpy ( sBuf, pWord, iLen );
+		sBuf[iLen] = '\0';
+
+	} else if ( m_bInfixes )
 	{
 		////////////////////////////////////
 		// infix or mixed infix+prefix mode
