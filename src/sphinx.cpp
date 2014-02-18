dfc66992c7be5f7c5d6505dd4eb18637c76d387c
tomat <tomat@406a0c4d-033a-0410-8de8-e80135713968>
2014-02-18 10:48:30 +0000

    fixed #1875 got rid of indexing stemmed forms by keyword infix dictionary added regressions to test 41
    git-svn-id: svn://svn.sphinxsearch.com/sphinx/branches/rel21@4561 406a0c4d-033a-0410-8de8-e80135713968

--- src/sphinx.cpp
+++ src/sphinx.cpp
@@ -20677,7 +20677,7 @@ protected:
 
 public:
 					InfixBuilder_c();
-	virtual void	AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint );
+	virtual void	AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint, bool bHasMorphology );
 	virtual void	SaveEntries ( CSphWriter & wrDict );
 	virtual int		SaveEntryBlocks ( CSphWriter & wrDict );
 	virtual int		GetBlocksWordsSize () const { return m_dBlocksWords.GetLength(); }
@@ -20737,8 +20737,17 @@ InfixBuilder_c<SIZE>::InfixBuilder_c()
 
 /// single-byte case, 2-dword infixes
 template<>
-void InfixBuilder_c<2>::AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint )
+void InfixBuilder_c<2>::AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint, bool bHasMorphology )
 {
+	if ( bHasMorphology && *pWord!=MAGIC_WORD_HEAD_NONSTEMMED )
+		return;
+
+	if ( *pWord<0x20 ) // skip heading magic chars, like NONSTEMMED maker
+	{
+		pWord++;
+		iWordLength--;
+	}
+
 	Infix_t<2> sKey;
 	for ( int p=0; p<=iWordLength-2; p++ )
 	{
@@ -20768,8 +20777,17 @@ void InfixBuilder_c<2>::AddWord ( const BYTE * pWord, int iWordLength, int iChec
 
 /// UTF-8 case, 3/5-dword infixes
 template < int SIZE >
-void InfixBuilder_c<SIZE>::AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint )
+void InfixBuilder_c<SIZE>::AddWord ( const BYTE * pWord, int iWordLength, int iCheckpoint, bool bHasMorphology )
 {
+	if ( bHasMorphology && *pWord!=MAGIC_WORD_HEAD_NONSTEMMED )
+		return;
+
+	if ( *pWord<0x20 ) // skip heading magic chars, like NONSTEMMED maker
+	{
+		pWord++;
+		iWordLength--;
+	}
+
 	int iCodes = 0; // codepoints in current word
 	BYTE dBytes[SPH_MAX_WORD_LEN+1]; // byte offset for each codepoints
 
@@ -21568,6 +21586,7 @@ bool CSphDictKeywords::DictEnd ( DictHeader_t * pHeader, int iMemLimit, CSphStri
 		qWords.Push ( tEntry );
 	}
 
+	bool bHasMorphology = HasMorphology();
 	CSphKeywordDeltaWriter tLastKeyword;
 	int iWords = 0;
 	while ( qWords.GetLength() )
@@ -21614,7 +21633,7 @@ bool CSphDictKeywords::DictEnd ( DictHeader_t * pHeader, int iMemLimit, CSphStri
 
 		// build infixes
 		if ( pInfixer )
-			pInfixer->AddWord ( (const BYTE*)tWord.m_sKeyword, iLen, m_dCheckpoints.GetLength() );
+			pInfixer->AddWord ( (const BYTE*)tWord.m_sKeyword, iLen, m_dCheckpoints.GetLength(), bHasMorphology );
 
 		// next
 		int iBin = tWord.m_iBlock;
@@ -29344,6 +29363,7 @@ void sphDictBuildInfixes ( const char * sPath )
 	if ( !pInfixer )
 		sphDie ( "infix upgrade: %s", sError.cstr() );
 
+	bool bHasMorphology = !tDictSettings.m_sMorphology.IsEmpty();
 	// scan all dict entries, generate infixes
 	// (in a separate block, so that tDictReader gets destroyed, and file closed)
 	{
@@ -29355,7 +29375,7 @@ void sphDictBuildInfixes ( const char * sPath )
 		{
 			const BYTE * sWord = tDictReader.GetWord();
 			int iLen = strlen ( (const char *)sWord );
-			pInfixer->AddWord ( sWord, iLen, tDictReader.GetCheckpoint() );
+			pInfixer->AddWord ( sWord, iLen, tDictReader.GetCheckpoint(), bHasMorphology );
 		}
 	}
 
